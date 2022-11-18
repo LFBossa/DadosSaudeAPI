7a548be348be777b0d32757e038a0026da9657b2
Bossa <l.f.bossa@ufsc.br>
2022-11-18 10:59:06 -0300

    tentativa de corrigir

--- src/importaSISAB.py
+++ src/importaSISAB.py
@@ -2,18 +2,30 @@ import requests
 from time import sleep
 import json
 
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-def get_SUSera(headers_raw, ano_dados,mes_dados):
+def get_SUSera(headers_raw, cookies, ano_dados,mes_dados):
     """Vamos importar dados do SUS na MARRA""" 
-    headers = {key: val.strip() for (key, val) in [ x.split(":", maxsplit=1) for x in headers_raw.split("\n")]}
+    line_headers = headers_raw.split("\n")
+    headers = {key: val.strip() for (key, val) in [ x.split(":", maxsplit=1) for x in line_headers[1:]]}
+    _, urlpath, _ = line_headers[0].split(" ")
     url = headers["Referer"]
     payload_raw = """j_idt44=j_idt44
 lsCid=
 dtBasicExample_length=10
 lsSigtap=
 td-ls-sigtap_length=10
-unidGeo=estado
-estados=SC
+unidGeo=brasil
+j_idt76=202207
 selectLinha=MUN.CO_MUNICIPIO_IBGE
 selectcoluna=PCA
 idadeInicio=0
@@ -21,38 +33,56 @@ idadeFim=0
 tpIdade=
 tpProducao=4 
 tabela_length=10
-javax.faces.ViewState=-618364827450665268:-2729416449945736178
+javax.faces.ViewState=7902303290776419962%3A635397846698827681
 j_idt192=j_idt192"""
     payload = {key: val.strip() for (key, val) in [ x.split("=", maxsplit=1) for x in payload_raw.split("\n")]}
     payload["j_idt76"] =  ["{ano}{:02d}".format(mes_dados,ano=ano_dados)]
-    payload["condicaoAvaliada"] = ["ABP{:03d}".format(x) for x in [9, 6, 10, 5, 7, 11, 12, 14, 18, 17, 23, 22]]
-    payload["tipoAtendimento"] = [1,2,4,5,6]
+    payload["condicaoAvaliada"] = ["ABP{:03d}".format(x) for x in [9, 6, 10, 5, 7, 11, 12, 14, 18, 17, 23, 22]] 
     # O seu token está visível apenas pra você
-    response = requests.post(url, headers=headers,data=payload)
+    print(url)
+    response = requests.post(url, headers=headers, data=payload)
     if response.status_code == 200:
         return response.text
     else:
         return response.status_code
 
+
+def multiline_input(message):
+    print(f"{message} {bcolors.UNDERLINE}[Ctrl-D or Ctrl-Z ( windows ) to save it.]{bcolors.ENDC}")
+    contents = []
+    while True:
+        try:
+            line = input()
+        except EOFError:
+            break
+        contents.append(line)
+    return "\n".join(contents)
+
 if __name__ == "__main__":
-    headers = """Host: sisab.saude.gov.br
-User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:99.0) Gecko/20100101 Firefox/99.0
+    #20headers = multiline_input("Insira o header: ")
+    headers = """POST /paginas/acessoRestrito/relatorio/federal/saude/RelSauProducao.xhtml HTTP/1.1
+Host: sisab.saude.gov.br
+User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:107.0) Gecko/20100101 Firefox/107.0
 Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
 Accept-Language: pt-BR,pt;q=0.8,en-US;q=0.5,en;q=0.3
 Accept-Encoding: gzip, deflate, br
 Content-Type: application/x-www-form-urlencoded
-Content-Length: 713
+Content-Length: 310
 Origin: https://sisab.saude.gov.br
 Connection: keep-alive
-Referer: https://sisab.saude.gov.br/paginas/acessoRestrito/relatorio/federal/saude/RelSauProducao.xhtml;jsessionid=x+yOlKywigRisCru9HiAAsUl
-Cookie: _ga=GA1.3.600333197.1649334263; JSESSIONID=x+yOlKywigRisCru9HiAAsUl; BIGipServersisab_prod=1946230188.22560.0000; TS0116db68=01f6470b2fafdf7d989cacc4e92a2f8e91cfb807dcec0c32d3b0ba111a5c7ea26c9a70f51e0704c42b93761623982bedfb3a84d6c021bf62f335dd9c8478af00a7ec38f4caa8cbbdb4ee03e2686f9d6af337532456; _gid=GA1.3.500987628.1649959631
+Referer: https://sisab.saude.gov.br/paginas/acessoRestrito/relatorio/federal/saude/RelSauProducao.xhtml;jsessionid=C8BeSZyD48Kxw+-EfVIkn6AS
+Cookie: JSESSIONID=uD9jz-lvJ2Neuo8zPojQwmHn; BIGipServerpool_sisab_jboss=!LjAP3jK4LYSgF3Oi4dOS8fa1J/wqqRxmdZBiRlRGAKLDcNuZsctBlJt1UJc/RRWUBNNmHGX+yI8Znak=
 Upgrade-Insecure-Requests: 1
 Sec-Fetch-Dest: document
 Sec-Fetch-Mode: navigate
 Sec-Fetch-Site: same-origin
 Sec-Fetch-User: ?1"""
-    referencia = input("Digite a referência: ")
-    ano = int(referencia[:4])
-    mes = int(referencia[4:6])
+    cookies = {
+		"BIGipServerpool_sisab_jboss": "!LjAP3jK4LYSgF3Oi4dOS8fa1J/wqqRxmdZBiRlRGAKLDcNuZsctBlJt1UJc/RRWUBNNmHGX+yI8Znak=",
+		"JSESSIONID": "uD9jz-lvJ2Neuo8zPojQwmHn"
+	}
+    #referencia = input("Digite a referência (YYYYMM): ")
+    #ano = int(referencia[:4])
+    #mes = int(referencia[4:6])
+    print(get_SUSera(headers, None, 2022, 5))
 
-    print(ano, mes, sep="\t")
