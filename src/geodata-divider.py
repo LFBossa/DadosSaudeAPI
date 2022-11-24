1d716449e34ce7732b4f1d416c7e1bbec5d35fa8
Bossa <l.f.bossa@ufsc.br>
2022-11-24 12:50:48 -0300

    Dados vindos direto do IBGE

--- src/geodata-divider.py
+++ src/geodata-divider.py
@@ -1,44 +1,45 @@
 import re
 import json
 from pathlib import Path
+import requests
 
 RAW_DATA_PATH = Path(__file__).parent.parent / "raw_data"
 DATA_PATH = Path(__file__).parent.parent / "data"
  
-
+# https://servicodados.ibge.gov.br/api/docs/malhas?versao=2
 def extract_regex(linha : str) -> tuple:
     regex = re.compile(r"\*\s(\w{2})\s/\s(.*?)\s-\s\[geojson/geojs-(\d{2})-")
     return regex.match(linha)
 
-def load_codigos_estados() -> dict:
-    dados_estados = {}
-    with open(RAW_DATA_PATH / "geodata-br/README.md") as fp:
-        for linha in fp:
-            resultado = extract_regex(linha)
-            if resultado:
-                uf, estado, cod  = resultado.group(1,2,3)
-                dados_estados[estado.strip()] = (uf, cod)
-    return dados_estados
+def json_or_none( URL : str ) -> str:
+    pedido = requests.get(URL)
+    if pedido.status_code == 200:
+        return json.loads(pedido.text)
 
-def load_brazil() -> dict:
-    with open( RAW_DATA_PATH / "geodata-br/brazil-geodata-raw.geojson") as fp:
-        dados = json.load(fp)
-    return dados
- 
+def get_cidades() -> dict:
+    return json_or_none("https://servicodados.ibge.gov.br/api/v1/localidades/municipios")
+
+def get_estados() -> dict:
+    return json_or_none("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    
+
+def get_topologia( estado_id : int) -> dict:
+    URL = f"https://servicodados.ibge.gov.br/api/v2/malhas/{estado_id}?resolucao=5&formato=application/json"
+    return json_or_none(URL)
 
 if __name__ == "__main__":
-    estados = load_codigos_estados()
-    brazil_geojson =  load_brazil()
-    estados_geojson = {key: [] for key in estados.keys()}
-    for elem in brazil_geojson['features']:
-        uf = elem['properties']['UF']
-        estados_geojson[uf].append(elem)
-    
-    for key, val in estados.items():
-        uf, _ = val
-        modelo = {"type":"FeatureCollection", "features": [] }
-        modelo["features"] = estados_geojson[key] 
-        with open(DATA_PATH / "geodata" / f"{uf}.geojson" , "w") as fp:
-            json.dump(modelo, fp, ensure_ascii=False)
-    
+    estados = get_estados()
+    cidades_lista = get_cidades()
+    cidades_dict = {}
+    for x in cidades_lista:
+        cidades_dict[str(x["id"])] = x
+    for estado in estados:
+        topojson = get_topologia(estado["id"])
+        for x in topojson["objects"]["foo"]["geometries"]:
+            ibgeid = x["properties"]["codarea"]
+            x["properties"]["municipio"] = cidades_dict[ibgeid]["nome"]
+        caminho = DATA_PATH / f"geodata/{estado['sigla']}.topojson"
+        with open(caminho, "w") as fp:
+            json.dump(topojson, fp, ensure_ascii=False) 
+      
     
